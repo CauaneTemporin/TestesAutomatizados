3e8a9c75d287fe3c16dac4847f37cbb377a7d34b
Cauane Temporin <temporin.cauane@gmail.com>
2024-03-29 17:39:43 -0300

    Adicionando o teste reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar com TDD

--- TDD/rh_recursos/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ TDD/rh_recursos/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -1,5 +1,21 @@
 package br.com.alura.tdd.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
 public class ReajusteServiceTest {
 
+	@Test
+	public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar() {
+		ReajusteService service = new ReajusteService();
+		Funcionario funcionario = new Funcionario("Cauane", LocalDate.now(), new BigDecimal("1000.00"));
+		
+		service.concederReajuste(funcionario, Desepenho.A_DESEJAR);
+		Assertions.assertEquals(new BigDecimal(1030.00), funcionario.getSalario());
+	}
 }
