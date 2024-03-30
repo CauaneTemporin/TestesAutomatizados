238131864e45ceebbc03a54785e48ee9a31ee5c4
Cauane Temporin <temporin.cauane@gmail.com>
2024-03-29 22:21:11 -0300

    Lidando com exceptions no JUnit - exemplo 1

--- TDD/rh_recursos/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ TDD/rh_recursos/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -11,23 +11,23 @@ import br.com.alura.tdd.modelo.Funcionario;
 class BonusServiceTest {
 
 	@Test
-	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+	void bonusDevceriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Cauane", LocalDate.now(), new BigDecimal("25000")));
-		Assertions.assertEquals(new BigDecimal("0.00"), bonus);
+		Assertions.assertThrows(IllegalArgumentException.class,
+				() -> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
 	}
-	
+
 	@Test
 	void bonusDeveriaSer10PorCentoDoSalario() {
 		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(new Funcionario("Cauane", LocalDate.now(), new BigDecimal("2500")));
 		Assertions.assertEquals(new BigDecimal("250.00"), bonus);
 	}
-	
+
 	@Test
 	void bonusDeveriaSer10PorCentoParaSalarioDeExatamente10000() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Cauane", LocalDate.now(), new BigDecimal("10000")));
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Cauane", LocalDate.now(), new BigDecimal("1000")));
 		Assertions.assertEquals(new BigDecimal("100.00"), bonus);
 	}
 }
